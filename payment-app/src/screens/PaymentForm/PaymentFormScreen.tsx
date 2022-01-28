329edeb9474964c3643724895cf7fa3bb31a1a17
gunelaliyeva <gunel.aliyeva.3011@gmail.com>
2022-01-28 23:27:18 +0400

    Payment Form Screen created

--- payment-app/src/screens/PaymentForm/PaymentFormScreen.tsx
+++ payment-app/src/screens/PaymentForm/PaymentFormScreen.tsx
@@ -4,17 +4,29 @@ import {InputGroup, Dropdown, DropdownButton, Button, Form} from "react-bootstra
 import './Paymentform.scss';
 import CategoryListButton from "../../components/CategoryListButton";
 import {useParams} from "react-router-dom";
+import axios from "axios";
 
 const PaymentFormScreen = () => {
     let {categoryId, providerId} = useParams();
     let currencyArray = ['AZN', 'USD', 'TL', 'RUB'];
-    let selectedCategory: {name: string, providers: [any]};
+    const [selectedCategory, setSelectCategory] = useState();
     const [categories, setCategories] = useState<any[]>([]);
     const [providers, setProviders] = useState<any[]>([]);
     const [fields, setFields] = useState<any[]>([]);
     const [options, setOptions] = useState<any[]>([]);
-    const [prefix, setPrefix] = useState<{ k: any, v: any }>({k: '', v: ''});
-    const [selectedCurrency, setSelectedCurrency] = useState(currencyArray[0]);
+    const [fieldValues, setFieldValues] = useState({
+        subscriberNumber: '',
+        prefix: {
+            k: "0",
+            v: ""
+        },
+        amount: '',
+        currency: currencyArray[0],
+        cardNumber: '',
+        expMonth: '',
+        expYear: '',
+        cvv: ''
+    })
 
     useEffect(() => {
         const res = JSON.parse(localStorage.getItem('categories') as string);
@@ -22,8 +34,12 @@ const PaymentFormScreen = () => {
     }, []);
 
     useEffect(() => {
-        selectedCategory = categories.find(category => category["_id"] === categoryId);
-        if (selectedCategory) setProviders(selectedCategory.providers);
+        const res = categories.find(category => category["_id"] === categoryId);
+        if (res) {
+            setProviders(res.providers);
+            setSelectCategory(res.name);
+            // console.log(selectedCategory === "Mobile");
+        }
         // setFields([...[...categories].find(category => category["_id"] === categoryId)
         //     .providers].find(provider => provider["_id"] === providerId).fields);
     }, [categories]);
@@ -38,60 +54,100 @@ const PaymentFormScreen = () => {
     }, [fields]);
 
     useEffect(() => {
-        setPrefix(options ? options[0] : {k: '', v: ''});
+
+        if (options) {
+            if (options[0]) {
+                setPrefixHandler(options[0]);
+                // console.log(options[0].v);
+            }
+        }
     }, [options]);
+
+    const setPrefixHandler = (val: { k: string, v: string }) => {
+        fieldValuesHandler("prefix", val);
+
+    }
+
+    const fieldValuesHandler = (name: string, val: string | { k: string, v: string }) => {
+        setFieldValues({
+            ...fieldValues,
+            [name]: val
+        });
+    }
+
+    const payHandler = () => {
+        console.log(fieldValues);
+        axios.post('http://localhost:8080/payment/new', {
+            details: [],
+            amount: {
+                value: fieldValues.amount,
+                currency: fieldValues.currency
+            }
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
     return (
         <>
             <CategoryListButton/>
             <div className="flex-center page-area">
                 <div className="card card-auto flex-between flex-column">
                     <div className="flex-between flex-column gap-20 width-100">
-                        {
-                            //@ts-ignore
-                            selectedCategory?.name !== "Mobile" ?
-                                <InputGroup>
+                        <InputGroup>
+                            {
+                                selectedCategory === "Mobile" ?
                                     <DropdownButton
                                         variant="outline-secondary"
-                                        title={prefix?.v}
+                                        title={fieldValues.prefix.v}
                                         id="input-group-dropdown-1"
                                     >
                                         {
                                             options?.map(option => {
-                                                return <Dropdown.Item onClick={e => setPrefix(option)}
-                                                                      key={option["_id"]}>{option.v}</Dropdown.Item>
+                                                return <Dropdown.Item
+                                                    onClick={() => setPrefixHandler(option)}
+                                                    key={option["_id"]}>{option.v}</Dropdown.Item>
                                             })
                                         }
                                     </DropdownButton>
-                                    <Form.Control placeholder="Number"/>
-                                </InputGroup>
-                                : <Form.Control type="text" placeholder="Subscriber Number"/>
-                        }
+                                    : null
+                            }
+                            <Form.Control placeholder="Subscriber Number" type="text"
+                                          onChange={e => fieldValuesHandler('subscriberNumber', e.target.value)}/>
+                        </InputGroup>
 
                         <InputGroup>
-                            <Form.Control placeholder="Amount" type="text"/>
+                            <Form.Control placeholder="Amount" type="text"
+                                          onChange={e => fieldValuesHandler('amount', e.target.value)}/>
                             <DropdownButton
                                 variant="outline-secondary"
-                                title={selectedCurrency}
+                                title={fieldValues.currency}
                                 id="input-group-dropdown-2"
                                 align="end"
                             >
                                 {
                                     currencyArray.map(currency => {
-                                        return <Dropdown.Item onClick={val => setSelectedCurrency(currency)}
+                                        return <Dropdown.Item onClick={() => fieldValuesHandler('currency', currency)}
                                                               key={currency}>{currency}</Dropdown.Item>
                                     })
                                 }
                             </DropdownButton>
                         </InputGroup>
-                        <Form.Control type="text" placeholder="Card Number"/>
+                        <Form.Control type="text" placeholder="Card Number"
+                                      onChange={e => fieldValuesHandler('cardNumber', e.target.value)}/>
                         <div className="flex-between gap-20 width-100">
-                            <Form.Control type="text" placeholder="Expiration Month"/>
-                            <Form.Control type="text" placeholder="Expiration Year"/>
-                            <Form.Control type="text" placeholder="CVV"/>
+                            <Form.Control type="text" placeholder="Expiration Month"
+                                          onChange={e => fieldValuesHandler('expMonth', e.target.value)}/>
+                            <Form.Control type="text" placeholder="Expiration Year"
+                                          onChange={e => fieldValuesHandler('expYear', e.target.value)}/>
+                            <Form.Control type="text" placeholder="CVV"
+                                          onChange={e => fieldValuesHandler('cvv', e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex-end width-100">
-                        <Button variant="outline-success" className="col-3">Pay</Button>
+                        <Button variant="outline-success" className="col-3" onClick={payHandler}>Pay</Button>
                     </div>
                 </div>
             </div>
